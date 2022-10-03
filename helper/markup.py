a8362879d5437157df708b03158c6b27dcf87183
Vishal R <67860986+Mrvishal2k2@users.noreply.github.com>
2022-10-04 00:42:36 +0530

    Update markup.py

--- helper/markup.py
+++ helper/markup.py
@@ -2,7 +2,7 @@
 
 def MakeCaptchaMarkup(markup, _number, sign):
     __markup = markup
-    for i in __markup:
+    for i in markup:
         for k in i:
             if k["text"] == _number:
                 k["text"] = f"{sign}"
